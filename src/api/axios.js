8c68f0aedaa1c26c135f856343d2e8fb3895b5bd
Parshuram Kumar <yadavparshuram991@gmail.com>
2026-08-08 03:48:42 +0530

    added backend deployed url

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/',
+    baseURL:import.meta.env.VITE_API_BASE_URL || 'https://devmind-backend-9tu4.onrender.com',
 });
 
 
